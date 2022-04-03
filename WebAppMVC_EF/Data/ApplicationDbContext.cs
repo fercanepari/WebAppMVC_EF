20fe17edf8f3f279b3f0a0ec067044d48dbaaa99
ferca <ferca@DESKTOP-9H4L81S>
2022-04-02 23:58:56 -0300

    Add project files.

---
+++ WebAppMVC_EF/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebAppMVC_EF.Models;
+
+namespace WebAppMVC_EF.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Product> Product { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ WebAppMVC_EF/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace WebAppMVC_EF.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Product()
+        {
+
+        }
+    }
+}
