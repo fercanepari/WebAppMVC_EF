9771fb86ee5d4e1683bfde7164d4f40b3956477f
fercanepari <fercanepari@gmail.com>
2022-04-03 01:34:34 -0300

    Date field added and EF migrations

--- WebAppMVC_EF/Data/Migrations/20220403042700_creationDateAdded.Designer.cs
+++ WebAppMVC_EF/Data/Migrations/20220403042700_creationDateAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAppMVC_EF.Data;
 
@@ -11,9 +12,10 @@ using WebAppMVC_EF.Data;
 namespace WebAppMVC_EF.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220403042700_creationDateAdded")]
+    partial class creationDateAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
